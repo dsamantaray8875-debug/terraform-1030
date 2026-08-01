937c4ba0581fe51fc1a5f9d362fd5444167f36d2
Deepak Kumar Samantaray <dsamantaray8875@gmail.com>
2026-08-02 02:07:13 +0530

    Added Jenkins Pipeline

--- project-5/backend.tf
+++ project-5/backend.tf
@@ -2,11 +2,11 @@ terraform {
 
   backend "s3" {
 
-    bucket         = "deepak-terraform-state-2026"
+    bucket         = "deepak-terraform-state-2025"
 
     key            = "project-5/terraform.tfstate"
 
-    region         = "us-east-2"
+    region         = "us-west-2"
 
     dynamodb_table = "terraform-lock"
 
